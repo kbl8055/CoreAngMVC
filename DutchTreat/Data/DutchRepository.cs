7a3cd2a52ce162e0b6230cbf49425e7f2abad7ff
Kenneth B. Laforteza <kbl80_55@yahoo.com>
2019-01-25 16:03:26 +0800

    LogLevel

--- DutchTreat/Data/DutchRepository.cs
+++ DutchTreat/Data/DutchRepository.cs
@@ -11,13 +11,16 @@ namespace DutchTreat.Data
   public class DutchRepository : IDutchRepository
   {
     private readonly DutchContext _context;
+    private readonly ILogger<DutchRepository> _logger;
 
-    public DutchRepository(DutchContext context, ILogger<DutchRepository>)
+    public DutchRepository(DutchContext context, ILogger<DutchRepository> logger)
     {
       this._context = context;
+      this._logger = logger;
     }
     public IEnumerable<Product> GetAllProducts()
     {
+      _logger.LogInformation("GetAllProducts called");
       return _context.Products.OrderBy(p => p.Title).ToList();
     }
 
